f3c10076537330fb4d5fa210cd19393a05f867a3
ErwinPo <A01734881@tec.mx>
2023-11-02 20:04:04 -0600

    No jala el main feed con queries del firestore :(

--- app/src/main/java/com/example/voluntaria360/EventosFragment.java
+++ app/src/main/java/com/example/voluntaria360/EventosFragment.java
@@ -7,11 +7,16 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
 
 public class EventosFragment extends Fragment {
 
-    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.eventosfragment, null);
