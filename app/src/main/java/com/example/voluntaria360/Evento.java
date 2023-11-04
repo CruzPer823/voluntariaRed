6b61234e6ef956329320e51aac2478a543225421
ErwinPo <A01734881@tec.mx>
2023-11-03 19:10:50 -0600

    created eventofragmentactivity, hace queries y funciona bien, falta implementacion en main feed

--- app/src/main/java/com/example/voluntaria360/Evento.java
+++ app/src/main/java/com/example/voluntaria360/Evento.java
@@ -1,19 +1,37 @@
 package com.example.voluntaria360;
 
-import java.util.Date;
-
 public class Evento {
-    String descripcion, titulo, fecha;
+    String titulo,fecha,descripcion;
 
-    public String getDescripcion() {
-        return descripcion;
+    public Evento(){}
+
+    public Evento(String titulo, String fecha, String descripcion) {
+        this.titulo = titulo;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
     }
 
     public String getTitulo() {
         return titulo;
     }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
     public String getFecha() {
         return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 }
