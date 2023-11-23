7d68c2e106bc83ad0748e4a8566f0afd2d932089
ErwinPo <A01734881@tec.mx>
2023-11-22 19:03:52 -0600

    Saved eventos, not working

---
+++ app/src/main/java/com/example/voluntaria360/SavedEventoFragmentActivity.java
@@ -0,0 +1,139 @@
+package com.example.voluntaria360;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class SavedEventoFragmentActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<Evento> eventoList;
+    ArrayList<Evento> savedEventoArrayList;
+    ArrayList<HorasVoluntarios> registros;
+    SavedEventosAdapter eventosAdapter;
+    FirebaseFirestore db;
+    FirebaseFirestore db2;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_savedeventofragment);
+
+/*
+        BottomNavigationView navigation = findViewById(R.id.barra_Menu);
+        navigation.setSelectedItemId(R.id.thirdFragment);
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+                if (itemId == R.id.firstFragment) {
+                    startActivity(new Intent(getApplicationContext(), EventoFragmentActivity.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                } else if (itemId == R.id.secondFragment) {
+                    startActivity(new Intent(getApplicationContext(), activity_sos.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                } else if (itemId == R.id.thirdFragment) {
+                    return true;
+                }
+                return false;
+            }
+        });
+*/
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("LocoOo");
+        progressDialog.setMessage("LocOoOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+        progressDialog.show();
+
+
+        recyclerView = findViewById(R.id.testrecyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        db = FirebaseFirestore.getInstance();
+        db2 = FirebaseFirestore.getInstance();
+        eventoList = new ArrayList<Evento>();
+        savedEventoArrayList = new ArrayList<Evento>();
+        eventosAdapter = new SavedEventosAdapter(SavedEventoFragmentActivity.this, savedEventoArrayList);
+
+        recyclerView.setAdapter(eventosAdapter);
+
+        EventChangeListener();
+    }
+
+    private void EventChangeListener() {
+
+        db2.collection("horasVoluntarios").whereEqualTo("idVoluntario", FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                        if(error != null){
+
+                            if(progressDialog.isShowing()){
+                                progressDialog.dismiss();
+                            }
+                            Log.e("Firestore error", error.getMessage());
+                            return;
+                        }
+
+                        for(DocumentChange dc : value.getDocumentChanges()){
+                            if(dc.getType() == DocumentChange.Type.ADDED ) {
+                                HorasVoluntarios doc = dc.getDocument().toObject(HorasVoluntarios.class);
+                                registros.add(doc);
+                            }
+                        }
+
+                        eventosAdapter.notifyDataSetChanged();
+                        if(progressDialog.isShowing()){
+                            progressDialog.dismiss();
+                        }
+                    }
+                });
+
+        for(HorasVoluntarios doc : registros){
+            DocumentReference docRef = db.collection("anuncios").document(doc.evento);
+            docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    Evento evento = documentSnapshot.toObject(Evento.class);
+                    evento.setTitulo("hola");
+                    savedEventoArrayList.add(evento);
+                }
+            });
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/voluntaria360/SavedEventosAdapter.java
@@ -0,0 +1,91 @@
+package com.example.voluntaria360;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SavedEventosAdapter extends RecyclerView.Adapter<SavedEventosAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<Evento> eventoList;
+    FirebaseFirestore db;
+
+    public SavedEventosAdapter(Context context, ArrayList<Evento> eventoList) {
+        this.context = context;
+        this.eventoList = eventoList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(context).inflate(R.layout.eventositem, parent, false);
+
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Evento evento = eventoList.get(position);
+        holder.titulo.setText(evento.titulo);
+        holder.fecha.setText(evento.fechaEvento);
+        holder.descripcion.setText(evento.descripcion);
+        holder.hrsMax.setText(evento.hrsMax + " horas");
+        Glide.with(context).load(evento.imagen).apply(new RequestOptions().override(holder.imagen.getMaxWidth(), holder.imagen.getMaxHeight())).into(holder.imagen);
+        db = FirebaseFirestore.getInstance();
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (holder.button.getText() != "Guardado"){
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("evento", evento.id);
+                    data.put("idVoluntario", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                    db.collection("horasVoluntarios").add(data);
+                }
+                holder.button.setText("Guardado");
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventoList.size();
+    }
+
+    public  static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView titulo,fecha,descripcion, hrsMax;
+        ImageView imagen;
+        Button button;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            button = itemView.findViewById(R.id.guardarbtn);
+            titulo = itemView.findViewById(R.id.eventotitulo);
+            fecha = itemView.findViewById(R.id.fecha);
+            descripcion = itemView.findViewById(R.id.descripcion);
+            imagen = itemView.findViewById(R.id.imagen);
+            hrsMax = itemView.findViewById(R.id.horas);
+        }
+    }
+}
